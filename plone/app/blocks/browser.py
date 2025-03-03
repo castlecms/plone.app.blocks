26837f3cc06acd945edac40c6b8bb71475c68e8f
Adam McNevin <miknevinas@gmail.com>
2025-03-03 10:29:39 -0800

    Re-enable index

--- plone/app/blocks/browser.py
+++ plone/app/blocks/browser.py
@@ -53,10 +53,9 @@ class ContentLayoutView(DefaultView):
         self.layout = self.get_layout()
         policy = theming_policy(self.request)
         settings = policy.getSettings()
-        # Python3 TODO - Some layouts don't render when run through index(). Not sure if still needed?
-        # try:
-        #     if not settings or settings.rules:
-        #         return self.index()
-        # except AttributeError:
-        #     pass
+        try:
+            if not settings or settings.rules:
+                return self.index()
+        except AttributeError:
+            pass
         return renderWithTheme(self.context, self.request, self.layout)
